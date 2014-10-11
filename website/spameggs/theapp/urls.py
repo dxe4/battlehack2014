259b08feb62967596a0dcc6fb21f7de976bc289e
swist <me@swistofon.pl>
2014-10-11 21:04:15 +0100

    wire in the urls

--- website/spameggs/theapp/urls.py
+++ website/spameggs/theapp/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import patterns, url
 from theapp.views import (CreateRequest, CreateResponse, 
-	ListResponses, AcceptAnswer, PurchaseTokens) 
+	ListResponses, ListRequests, AcceptAnswer, PurchaseTokens) 
 
 urlpatterns = patterns(
     '',
     url(r'^create_request$', CreateRequest.as_view(), name='create_request'),
     url(r'^create_response$', CreateResponse.as_view(), name='create_response'),
     url(r'^list_responses$', ListResponses.as_view(), name='list_responses'),
+    url(r'^list_requests$', ListRequests.as_view(), name='list_requests'),
     url(r'^accept_answer$', AcceptAnswer.as_view(), name='accept_answer'),
     url(r'^purchase_tokens$', PurchaseTokens.as_view(), name='purchase_tokens')
 )
