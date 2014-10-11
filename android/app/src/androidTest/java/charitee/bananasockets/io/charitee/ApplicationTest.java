0cd688d6064ee29b4a0eeb9cc0ba48b49c7639e1
Cédric Odi <cedric.odi@gmail.com>
2014-10-11 16:15:47 +0200

    first android commit

---
+++ android/app/src/androidTest/java/charitee/bananasockets/io/charitee/ApplicationTest.java
@@ -0,0 +1,13 @@
+package charitee.bananasockets.io.charitee;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
---
+++ android/app/src/main/java/charitee/bananasockets/io/charitee/Main.java
@@ -0,0 +1,14 @@
+package charitee.bananasockets.io.charitee;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import org.androidannotations.annotations.EActivity;
+
+
+@EActivity(R.layout.activity_main) public class Main extends Activity {
+
+
+}
