34e34efbe3a6f5654650d71663e3d12f9c7930dd
swist <me@swistofon.pl>
2014-10-11 16:35:27 +0100

    fix merge naming

--- website/spameggs/theapp/models.py
+++ website/spameggs/theapp/models.py
@@ -11,13 +11,16 @@ class UserRequest(models.Model):
     lat = models.FloatField()
     message = models.CharField(max_length=500)
     expires = models.DateTimeField()
+
     user = models.ForeignKey(User)
+
+    bounty = models.IntegerField(default=0)
 
 
 class UserResponse(models.Model):
     text = models.CharField(max_length=500)
     votes = models.IntegerField()
-    user_requset = models.ForeignKey(UserRequest,
+    user_request = models.ForeignKey(UserRequest,
                                      related_name='responses')
     user = models.ForeignKey(User)
-    bounty = models.IntegerField(default=0)
+    
