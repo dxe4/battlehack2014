b94fd7fb0d115e7da7067fbbe5d68da834ce2693
swist <me@swistofon.pl>
2014-10-11 18:30:00 +0100

    add urls in

--- website/spameggs/theapp/urls.py
+++ website/spameggs/theapp/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import patterns, url
-from theapp.views import CreateRequest
+from theapp.views import (CreateRequest, CreateResponse, 
+	ListResponses, AcceptAnswer) 
 
 urlpatterns = patterns(
     '',
     url(r'^create_request$', CreateRequest.as_view(), name='create_request'),
+    url(r'^create_response$', CreateResponse.as_view(), name='create_response'),
+    url(r'^list_responses$', ListResponses.as_view(), name='list_responses'),
+    url(r'^accept_answer$', AcceptAnswer.as_view(), name='accept_answer'),
 )
